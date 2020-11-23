02befb43efb693c382c03578210e6f2427e0cb30
upendra <joshiupendra1995>
2020-11-23 19:04:22 +0530

    OrderedDictionary

---
+++ OrderedDictionary.py
@@ -0,0 +1,15 @@
+from _collections import OrderedDict
+
+print("Before:\n")
+od = OrderedDict()
+od['a'] = 1
+od['b'] = 2
+od['c'] = 3
+od['d'] = 4
+for key, value in od.items():
+    print(key, value)
+
+print("\nAfter:\n")
+od['c'] = 5
+for key, value in od.items():
+    print(key, value)
