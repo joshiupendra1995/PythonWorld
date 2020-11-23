3a6f31c88ea76a0d23b02893e5b4c70b42a572e2
upendra <joshiupendra1995>
2020-11-23 20:21:52 +0530

    OrderedDictionary

---
+++ Intersection.py
@@ -0,0 +1,5 @@
+set1 = {1, 2, 3, 4}
+set2 = {3, 4, 5}
+
+print(set1.intersection(set2))  # intersect two sets
+print(set1.union(set2))  # union between two sets removing the duplicates
---
+++ NumpyExample.py
@@ -0,0 +1,9 @@
+import numpy as np
+
+list1 = [1, 2, 3, 5]
+list2 = [4, 5, 6, 7]
+
+list = list(list1 + list2)
+print(list)
+
+print(np.array(set(list)))
