315c8d8734addecf7c81157931d634becaf896bb
upendra <joshiupendra1995>
2020-11-25 20:24:55 +0530

    mysql create table

--- MySqlDb.py
+++ MySqlDb.py
@@ -1,12 +1,17 @@
 import mysql.connector
 
-conn = mysql.connector.connect(
-    host="localhost",
-    user="root",
-    password="root@123456789",
-    database="testdb"
-)
+cnx = mysql.connector.connect(user='root', password='root@123',
+                              host='localhost',
+                              database='test_db')
 
-cursor = conn.cursor()
+cursor = cnx.cursor()
+cursor.execute("drop table customers")
+cursor.execute("CREATE TABLE customers (name VARCHAR(255), address VARCHAR(255))")
 
-cursor.execute("CREATE TABLE devops (name VARCHAR(255), address VARCHAR(255))")
+print("Table created sucessfully!!")
+
+cursor.execute('''insert into customers values("upendra","pune")''')
+cnx.commit()
+print("Record inserted!!")
+
+cnx.close()
